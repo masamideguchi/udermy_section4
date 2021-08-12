ca9c0809b1d313777105243a6349fd97f56d60a8
出口 正美 <masami.deguchi@gmail.com>
2021-08-13 08:37:08 +0900

    テンプレート文字列

--- src/index.js
+++ src/index.js
@@ -47,3 +47,19 @@
 // val5.push("elphant");
 
 // console.log(val5);
+
+// **************************************************
+/**
+ * テンプレート文字列
+ */
+// const name = "まあ";
+// const age = 59;
+// // 私の名前はまあです。年齢は59歳です。
+
+// // 従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
+
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
