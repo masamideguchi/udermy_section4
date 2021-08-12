c31b5e97d8916f494239e3cf8fc8e1613e6be23d
出口 正美 <masami.deguchi@gmail.com>
2021-08-13 08:46:03 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -63,3 +63,25 @@
 // // テンプレート文字列を用いた方法
 // const message2 = `私の名前は${name}です。年齢は${age}です。`;
 // console.log(message2);
+
+// **************************************************
+/**
+ * アロー関数
+ */
+// 従来の関数
+// function func1(str) {
+//   return str;
+// }
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1xです。"));
+
+// // アロー関数
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2です"));
+
+// const func3 = (num1, num2) => num1 + num2;
+// console.log(func3(10, 22));
